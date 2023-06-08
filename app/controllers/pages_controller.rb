85b670e153c3a13b6f24b4c89ba0a8db2c26548e
Thibaut DE STEFANI <thibaut.destefani@gmail.com>
2023-06-08 18:13:10 +0200

    medical_care controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    @appointments = Appointment.where(user_id: current_user.id)
+    if params[:date].present?
+      @date_selected = Date.parse(params[:date])
+    else
+      @date_selected = Date.current
+    end
   end
 end
